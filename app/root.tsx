28c69fe6f6a0d7fb45cf8aecbbe29c8ef8bc5285
nyatinte <takumi.062757@gmail.com>
2024-02-07 13:38:18 +0900

    install shadcn

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,6 @@
-import { cssBundleHref } from '@remix-run/css-bundle'
-import type { LinksFunction } from '@remix-run/node'
+import styles from './tailwind.css';
+import { cssBundleHref } from '@remix-run/css-bundle';
+import type { LinksFunction } from '@remix-run/node';
 import {
   Links,
   LiveReload,
@@ -7,18 +8,19 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-} from '@remix-run/react'
+} from '@remix-run/react';
 
 export const links: LinksFunction = () => [
+  { rel: 'stylesheet', href: styles },
   ...(cssBundleHref ? [{ rel: 'stylesheet', href: cssBundleHref }] : []),
-]
+];
 
 export default function App() {
   return (
-    <html lang="en">
+    <html lang='en'>
       <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet='utf-8' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
         <Meta />
         <Links />
       </head>
@@ -29,5 +31,5 @@ export default function App() {
         <LiveReload />
       </body>
     </html>
-  )
+  );
 }
